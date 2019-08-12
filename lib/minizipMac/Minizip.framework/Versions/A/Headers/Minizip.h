f26fd3289148514e67c701d1ce257dde34fac567
danoon2 <danoon2@92b686db-d4d0-431c-8de6-2df121396777>
2019-08-12 22:56:12 +0000

    cpp: added minizip frame work to repo, now mac will be easier to build because no other libraries will be required

---
+++ lib/minizipMac/Minizip.framework/Versions/A/Headers/Minizip.h
@@ -0,0 +1,24 @@
+//
+//  Minizip.h
+//  Minizip
+//
+//  Created by Arthur Dexter on 2/6/16.
+//  Copyright © 2016 Arthur Dexter. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license.  See the LICENSE file for details.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Minizip.
+FOUNDATION_EXPORT double MinizipVersionNumber;
+
+//! Project version string for Minizip.
+FOUNDATION_EXPORT const unsigned char MinizipVersionString[];
+
+#import <Minizip/ioapi.h>
+#import <Minizip/ioapi_buf.h>
+#import <Minizip/ioapi_mem.h>
+#import <Minizip/unzip.h>
+#import <Minizip/zip.h>
