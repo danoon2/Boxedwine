92a8c43d7c9ae5e0f2b4088c7bc020d0dec65fb8
danoon2 <danoon2@92b686db-d4d0-431c-8de6-2df121396777>
2019-09-10 01:59:55 +0000

    BoxedwineUI: forgot to add a file

---
+++ tools/ui/src/wxWidgets/BoxedAppOptionsDlg.h
@@ -0,0 +1,28 @@
+#ifndef __BOXEDAPPOPTIONS_H__
+#define __BOXEDAPPOPTIONS_H__
+
+#include "BoxedApp.h"
+
+class wxListView;
+
+class BoxedAppOptionsDialog : public wxDialog {
+public:
+    BoxedAppOptionsDialog(wxWindow* parent, BoxedApp* app);
+
+private:
+    void OnDone(wxCommandEvent& event);    
+
+    BoxedApp* app;
+    wxTextCtrl* shortCutNameText;
+    wxTextCtrl* glExtText;
+    wxComboBox* screenResolutionCombobox;
+    wxComboBox* screenBppCombobox;
+    wxComboBox* scaleCombobox;
+    wxComboBox* scaleQualityCombobox;
+    wxCheckBox* fullScreenCheckBox;
+
+    wxDECLARE_EVENT_TABLE();
+    wxDECLARE_NO_COPY_CLASS(BoxedAppOptionsDialog);
+};
+
+#endif
