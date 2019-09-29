5e4d11ba7dcc95e8a3281c5e06d5fec0bcba4672
kevodwyer <kevodwyer@92b686db-d4d0-431c-8de6-2df121396777>
2019-09-29 11:42:57 +0000

    emscripten: clean up old emscripten build files

--- project/linux/buildjs_webgl.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-emcc -Wall -Wno-unused-function -I../../include ../../source/sdl/*.cpp ../../platform/linux/*.cpp ../../source/emulation/cpu/*.cpp ../../source/emulation/cpu/common/*.cpp ../../source/emulation/cpu/normal/*.cpp ../../source/emulation/softmmu/*.cpp ../../source/io/*.cpp ../../source/kernel/*.cpp ../../source/kernel/devs/*.cpp ../../source/kernel/proc/*.cpp ../../source/kernel/loader/*.cpp ../../source/util/*.cpp ../../source/opengl/sdl/*.cpp ../../source/opengl/*.cpp -o boxedwine.html -O2 -DUNALIGNED_MEMORY=1 -DUSE_MMU -DBOXEDWINE_ES -s LEGACY_GL_EMULATION=1 -s USE_SDL=1 -DGLH=\<GL/gl.h\> --preload-file root@/root -s TOTAL_MEMORY=603979776
-./packager.py
-mv package.js boxedwine.js
-gzip -9 boxedwine.data
-mv boxedwine.data.gz boxedwine.data
