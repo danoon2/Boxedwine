71e410b27913b48c9530ae51368057aa113ab227
James Bryant <james@danoon.net>
2020-04-24 10:06:44 -0700

    Fixed emscripten build

--- project/linux/createOfficialEmscriptenBuild.sh
+++ project/linux/createOfficialEmscriptenBuild.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -e
 sh buildjsfs_sdl2.sh $1
-mkdir build
-rm build/*
+mkdir -p build
+rm -f build/*
 cp boxedwine.html build/
 cp boxedwine.css build/
 cp boxedwine-shell.js build/
 cp boxedwine.js build/
 cp jszip.min.js build/
 cp browserfs.min.js build/
-cp boxedwine.html.mem build/
+#cp boxedwine.html.mem build/
 cp boxedwine.wasm build/
 cp buildfiles/* build/
 cd build
