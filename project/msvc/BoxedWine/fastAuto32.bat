c9cfcee797e2f2f54fbdd8ce17f58aa208d345fe
James Bryant <jwbryant72@yahoo.com>
2020-05-25 18:08:22 -0700

    Windows 32-bit automation fix

--- project/msvc/BoxedWine/fastAuto32.bat
+++ project/msvc/BoxedWine/fastAuto32.bat
@@ -4,4 +4,4 @@ curl -z auto/Wine-5.0.zip -o auto/Wine-5.0.zip http://208.113.132.187/automation
 curl -z auto/debian10.zip -o auto/debian10.zip http://208.113.132.187/automation/debian10.zip
 rmdir auto\abiword /s /Q
 unzip auto\abiword.zip -d auto
-x64\Release\Boxedwine -nosound -novideo -automation "%CD%\auto\abiword\play" -root "%CD%\auto\abiword\root" -zip "%CD%\auto\Wine-5.0.zip" -w /home/username/files/abiword /bin/wine /home/username/files/abiword/abiword
+Release\Boxedwine -nosound -novideo -automation "%CD%\auto\abiword\play" -root "%CD%\auto\abiword\root" -zip "%CD%\auto\Wine-5.0.zip" -w /home/username/files/abiword /bin/wine /home/username/files/abiword/abiword
