1b3453dce63cd094fe2557cc132289f5ba802b52
James Bryant <jwbryant72@yahoo.com>
2020-05-29 13:10:19 -0700

    Automation fix, the 32-bit and 64-bit scripts were clobbering each other

--- project/msvc/BoxedWine/fastAuto32.bat
+++ project/msvc/BoxedWine/fastAuto32.bat
@@ -1,7 +1,7 @@
 mkdir auto
-curl -z auto/abiword.zip -o auto/abiword.zip http://208.113.132.187/automation/auto-abiword-64.zip
+curl -z auto/auto-abiword-32.zip -o auto/auto-abiword-32.zip http://208.113.132.187/automation/auto-abiword-32.zip
 curl -z auto/Wine-5.0.zip -o auto/Wine-5.0.zip http://208.113.132.187/automation/Wine-5.0.zip
 curl -z auto/debian10.zip -o auto/debian10.zip http://208.113.132.187/automation/debian10.zip
 rmdir auto\abiword /s /Q
-unzip auto\abiword.zip -d auto
+unzip auto\auto-abiword-32.zip -d auto
 Release\Boxedwine -nosound -novideo -automation "%CD%\auto\abiword\play" -root "%CD%\auto\abiword\root" -zip "%CD%\auto\Wine-5.0.zip" -w /home/username/files/abiword /bin/wine /home/username/files/abiword/abiword
