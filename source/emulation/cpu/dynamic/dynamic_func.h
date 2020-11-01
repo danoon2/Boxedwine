danoon2/Boxedwine

commit 5e538449b3530a9ffbbb7411d5bf6f526b40a459

Fixed some bugs with the ARMv7 recompiler.

source/emulation/cpu/dynamic/dynamic_func.h
@@ -68,33 +68,34 @@ U32 cpuOffsetSrc(DynWidth width) {
     }
 }
 void dynamic_getCF(DynamicData* data);
+
 void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, DynWidth width, char inst, bool cf, bool store, const LazyFlags* flags) {
-    bool isJump = op->next->inst==JumpZ || op->next->inst==JumpNZ;
-    bool isSet = op->next->inst==SetZ_E8 || op->next->inst==SetZ_R8 || op->next->inst==SetNZ_E8 || op->next->inst==SetNZ_R8;
+    bool isJump = op->next->inst == JumpZ || op->next->inst == JumpNZ;
+    bool isSet = op->next->inst == SetZ_E8 || op->next->inst == SetZ_R8 || op->next->inst == SetNZ_E8 || op->next->inst == SetNZ_R8;
     bool needResultReg = isJump || isSet;
     DynReg jmpReg = DYN_NOT_SET;
     bool needsToSetFlags = op->needsToSetFlags();
 
     if (cf) {
         dynamic_getCF(data);
-    }   
+    }
     if (!needsToSetFlags && !store) {
         // I've seen a test followed by a cmp when running Quake 2.  Very weird
         INCREMENT_EIP(op->len);
         return;
     }
-    if (!needsToSetFlags || (needResultReg && DecodedOp::getNeededFlags(data->block, op->next, instructionInfo[op->inst].flagsSets)==0)) {
-        if (src == DYN_Reg) {            
-            if (dst==DYN_Reg) {
+    if (!needsToSetFlags || (needResultReg && DecodedOp::getNeededFlags(data->block, op->next, instructionInfo[op->inst].flagsSets) == 0)) {
+        if (src == DYN_Reg) {
+            if (dst == DYN_Reg) {
                 movToRegFromCpu(DYN_SRC, cpuOffset(op->rm, width), width);
                 if (cf) {
-                    if (width!=DYN_32bit) {
+                    if (width != DYN_32bit) {
                         movToRegFromReg(DYN_CALL_RESULT, width, DYN_CALL_RESULT, DYN_32bit, false);
                     }
                     instRegReg('+', DYN_SRC, DYN_CALL_RESULT, width, true);
                 }
                 if (needResultReg) {
-                    if (op->reg==op->rm && (flags == FLAGS_TEST8 || flags == FLAGS_TEST16 || flags == FLAGS_TEST32)) {
+                    if (op->reg == op->rm && (flags == FLAGS_TEST8 || flags == FLAGS_TEST16 || flags == FLAGS_TEST32)) {
                         jmpReg = DYN_SRC;
                     } else {
                         movToRegFromCpu(DYN_DEST, cpuOffset(op->reg, width), width);
@@ -107,33 +108,33 @@ void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, Dyn
                 } else {
                     instCPUReg(inst, cpuOffset(op->reg, width), DYN_SRC, width, true);
                 }
-            } else if (dst==DYN_Mem) {
+            } else if (dst == DYN_Mem) {
                 calculateEaa(op, DYN_ADDRESS);
                 movToRegFromCpu(DYN_SRC, cpuOffset(op->reg, width), width);
                 if (cf) {
-                    if (width!=DYN_32bit) {
+                    if (width != DYN_32bit) {
                         movToRegFromReg(DYN_CALL_RESULT, width, DYN_CALL_RESULT, DYN_32bit, false);
                     }
                     instRegReg('+', DYN_SRC, DYN_CALL_RESULT, width, true);
                 }
                 if (needResultReg) {
-                    movFromMem(width, DYN_ADDRESS, false);
-                    instRegReg(inst, DYN_CALL_RESULT, DYN_SRC, width, true);
+                    movFromMem(width, DYN_ADDRESS, !store);
+                    instRegReg(inst, DYN_SRC, DYN_CALL_RESULT, width, true);
                     if (store) {
-                        movToMemFromReg(DYN_ADDRESS, DYN_CALL_RESULT, width, true, false);
+                        movToMemFromReg(DYN_ADDRESS, DYN_SRC, width, true, false);
                     }
-                    jmpReg = DYN_CALL_RESULT;
+                    jmpReg = DYN_SRC;
                 } else {
                     instMemReg(inst, DYN_ADDRESS, DYN_SRC, width, true, true);
                 }
             }
-        } else if (src == DYN_Mem) {            
-            if (dst==DYN_Reg) {
+        } else if (src == DYN_Mem) {
+            if (dst == DYN_Reg) {
                 calculateEaa(op, DYN_ADDRESS);
                 if (cf) {
                     movToRegFromReg(DYN_DEST, width, DYN_CALL_RESULT, DYN_32bit, false);
                     movFromMem(width, DYN_ADDRESS, true);
-                    instRegReg('+', DYN_CALL_RESULT, DYN_DEST, width, true);                                        
+                    instRegReg('+', DYN_CALL_RESULT, DYN_DEST, width, true);
                 } else {
                     movFromMem(width, DYN_ADDRESS, true);
                 }
@@ -149,9 +150,9 @@ void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, Dyn
                 }
             }
         } else if (src == DYN_Const) {
-            if (dst==DYN_Reg) {
+            if (dst == DYN_Reg) {
                 if (cf) {
-                    if (width!=DYN_32bit) {
+                    if (width != DYN_32bit) {
                         movToRegFromReg(DYN_CALL_RESULT, width, DYN_CALL_RESULT, DYN_32bit, false);
                     }
                     instRegImm('+', DYN_CALL_RESULT, width, op->imm);
@@ -177,46 +178,49 @@ void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, Dyn
                         instCPUImm(inst, cpuOffset(op->reg, width), width, op->imm);
                     }
                 }
-            } else if (dst==DYN_Mem) {
-                calculateEaa(op, DYN_ADDRESS);      
+            } else if (dst == DYN_Mem) {
+                calculateEaa(op, DYN_ADDRESS);
                 if (cf) {
                     movToRegFromReg(DYN_SRC, width, DYN_CALL_RESULT, DYN_32bit, true);
                     instRegImm('+', DYN_SRC, width, op->imm);
                     if (needResultReg) {
-                        movFromMem(width, DYN_ADDRESS, false);
-                        instRegReg(inst, DYN_CALL_RESULT, DYN_SRC, width, true);
+                        movFromMem(width, DYN_ADDRESS, !store);
+                        instRegReg(inst, DYN_SRC, DYN_CALL_RESULT, width, true);
                         if (store) {
-                            movToMemFromReg(DYN_ADDRESS, DYN_CALL_RESULT, width, true, false);
+                            movToMemFromReg(DYN_ADDRESS, DYN_SRC, width, true, false);
                         }
-                        jmpReg = DYN_CALL_RESULT;
+                        jmpReg = DYN_SRC;
                     } else {
                         instMemReg(inst, DYN_ADDRESS, DYN_SRC, width, true, true);
                     }
                 } else {
                     if (needResultReg) {
-                        movFromMem(width, DYN_ADDRESS, false);
+                        movFromMem(width, DYN_ADDRESS, !store);
                         instRegImm(inst, DYN_CALL_RESULT, width, op->imm);
                         if (store) {
-                            movToMemFromReg(DYN_ADDRESS, DYN_CALL_RESULT, width, true, false);
+                            movToRegFromReg(DYN_SRC, width, DYN_CALL_RESULT, width, true); // movToMemFromReg might invalidate R0
+                            movToMemFromReg(DYN_ADDRESS, DYN_SRC, width, true, false);
+                            jmpReg = DYN_SRC;
+                        } else {
+                            jmpReg = DYN_CALL_RESULT;
                         }
-                        jmpReg = DYN_CALL_RESULT;
                     } else {
                         instMemImm(inst, DYN_ADDRESS, width, op->imm, true);
                     }
                 }
             }
-        }       
+        }
     } else {
         if (cf) {
             movToCpuFromReg(CPU_OFFSET_OF(oldCF), DYN_CALL_RESULT, DYN_32bit, false);
         }
-        if (src == DYN_Reg) {            
-            if (dst==DYN_Reg) {
+        if (src == DYN_Reg) {
+            if (dst == DYN_Reg) {
                 movToCpuFromCpu(cpuOffsetSrc(width), cpuOffset(op->rm, width), width, DYN_SRC, false);
                 movToCpuFromCpu(cpuOffsetDst(width), cpuOffset(op->reg, width), width, DYN_DEST, false);
-                
+
                 if (cf) {
-                    if (width!=DYN_32bit) {
+                    if (width != DYN_32bit) {
                         movToRegFromReg(DYN_CALL_RESULT, width, DYN_CALL_RESULT, DYN_32bit, false);
                     }
                     instRegReg('+', DYN_SRC, DYN_CALL_RESULT, width, true);
@@ -224,27 +228,29 @@ void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, Dyn
                 instRegReg(inst, DYN_DEST, DYN_SRC, width, true);
                 movToCpuFromReg(cpuOffsetResult(width), DYN_DEST, width, !store);
                 if (store) {
-                    movToCpuFromReg(cpuOffset(op->reg, width), DYN_DEST, width, true);                
+                    movToCpuFromReg(cpuOffset(op->reg, width), DYN_DEST, width, true);
                 }
-            } else if (dst==DYN_Mem) {
-                movToCpuFromCpu(cpuOffsetSrc(width), cpuOffset(op->reg, width), width, DYN_SRC, false);                                
+            } else if (dst == DYN_Mem) {
+                movToCpuFromCpu(cpuOffsetSrc(width), cpuOffset(op->reg, width), width, DYN_SRC, false);
                 if (cf) {
-                    if (width!=DYN_32bit) {
-                        movToRegFromReg(DYN_CALL_RESULT, width, DYN_CALL_RESULT, DYN_32bit, true);                                        
+                    if (width != DYN_32bit) {
+                        movToRegFromReg(DYN_CALL_RESULT, width, DYN_CALL_RESULT, DYN_32bit, true);
                     }
                     instRegReg('+', DYN_SRC, DYN_CALL_RESULT, width, true);
                 }
                 calculateEaa(op, DYN_ADDRESS);
                 movToCpuFromMem(cpuOffsetDst(width), width, DYN_ADDRESS, !store, false);
-                instRegReg(inst, DYN_CALL_RESULT, DYN_SRC, width, true);                
+                instRegReg(inst, DYN_CALL_RESULT, DYN_SRC, width, true);
                 movToCpuFromReg(cpuOffsetResult(width), DYN_CALL_RESULT, width, !store);
                 if (store) {
                     movToMemFromReg(DYN_ADDRESS, DYN_CALL_RESULT, width, true, true);
                 }
             }
-        } else if (src == DYN_Mem) {            
-            if (dst==DYN_Reg) {
-                movToRegFromReg(DYN_SRC, width, DYN_CALL_RESULT, DYN_32bit, true);
+        } else if (src == DYN_Mem) {
+            if (dst == DYN_Reg) {
+                if (cf) {
+                    movToRegFromReg(DYN_SRC, width, DYN_CALL_RESULT, DYN_32bit, true);
+                }
                 calculateEaa(op, DYN_ADDRESS);
                 movToCpuFromMem(cpuOffsetSrc(width), width, DYN_ADDRESS, true, false);
                 movToCpuFromCpu(cpuOffsetDst(width), cpuOffset(op->reg, width), width, DYN_DEST, false);
@@ -258,31 +264,31 @@ void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, Dyn
                 }
             }
         } else if (src == DYN_Const) {
-            if (dst==DYN_Reg) {
+            if (dst == DYN_Reg) {
                 movToCpu(cpuOffsetSrc(width), width, op->imm);
                 movToCpuFromCpu(cpuOffsetDst(width), cpuOffset(op->reg, width), width, DYN_DEST, false);
                 instRegImm(inst, DYN_DEST, width, op->imm);
-                if (cf) {               
-                    if (width!=DYN_32bit) {
+                if (cf) {
+                    if (width != DYN_32bit) {
                         movToRegFromReg(DYN_CALL_RESULT, width, DYN_CALL_RESULT, DYN_32bit, false);
                     }
-                    instRegReg(inst, DYN_DEST, DYN_CALL_RESULT, width, true);                    
+                    instRegReg(inst, DYN_DEST, DYN_CALL_RESULT, width, true);
                 }
                 movToCpuFromReg(cpuOffsetResult(width), DYN_DEST, width, !store);
                 if (store) {
                     movToCpuFromReg(cpuOffset(op->reg, width), DYN_DEST, width, true);
                 }
-            } else if (dst==DYN_Mem) {
-                calculateEaa(op, DYN_ADDRESS);      
+            } else if (dst == DYN_Mem) {
+                calculateEaa(op, DYN_ADDRESS);
                 movToCpu(cpuOffsetSrc(width), width, op->imm);
                 if (cf) {
-                    movToRegFromReg(DYN_SRC, width, DYN_CALL_RESULT, DYN_32bit, true);                    
+                    movToRegFromReg(DYN_SRC, width, DYN_CALL_RESULT, DYN_32bit, true);
                     movToCpuFromMem(cpuOffsetDst(width), width, DYN_ADDRESS, !store, false);
                     instRegImm('+', DYN_SRC, width, op->imm);
                     instRegReg(inst, DYN_CALL_RESULT, DYN_SRC, width, true);
-                } else {                    
+                } else {
                     movToCpuFromMem(cpuOffsetDst(width), width, DYN_ADDRESS, !store, false);
-                    instRegImm(inst, DYN_CALL_RESULT, width, op->imm);                    
+                    instRegImm(inst, DYN_CALL_RESULT, width, op->imm);
                 }
                 movToCpuFromReg(cpuOffsetResult(width), DYN_CALL_RESULT, width, !store);
                 if (store) {
@@ -294,22 +300,22 @@ void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, Dyn
         data->currentLazyFlags = flags;
     }
 
-    if (!needResultReg || jmpReg==DYN_NOT_SET) {
+    if (!needResultReg || jmpReg == DYN_NOT_SET) {
         INCREMENT_EIP(op->len);
     } else {
-        if (jmpReg==DYN_NOT_SET) {
+        if (jmpReg == DYN_NOT_SET) {
             kpanic("dynamic_arith did not properly set jmpReg");
         }
-        if (width!=DYN_32bit) {
+        if (width != DYN_32bit) {
             movToRegFromReg(jmpReg, DYN_32bit, jmpReg, width, false);
         }
         if (isJump) {
-            dynamic_jump(data, jmpReg, op->next->inst, op->len+op->next->len+op->next->imm, op->len+op->next->len);
+            dynamic_jump(data, jmpReg, op->next->inst, op->len + op->next->len + op->next->imm, op->len + op->next->len);
         } else if (isSet) {
             INCREMENT_EIP(op->len);
             op = op->next;
-            if (op->inst==SetZ_E8 || op->inst==SetNZ_E8) {                
-                startIf(jmpReg, (op->inst==SetZ_E8?DYN_EQUALS_ZERO:DYN_NOT_EQUALS_ZERO), true);
+            if (op->inst == SetZ_E8 || op->inst == SetNZ_E8) {
+                startIf(jmpReg, (op->inst == SetZ_E8 ? DYN_EQUALS_ZERO : DYN_NOT_EQUALS_ZERO), true);
                 movToReg(DYN_SRC, DYN_8bit, 1);
                 startElse();
                 movToReg(DYN_SRC, DYN_8bit, 0);
@@ -317,11 +323,11 @@ void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, Dyn
                 calculateEaa(op, DYN_ADDRESS);
                 movToMemFromReg(DYN_ADDRESS, DYN_SRC, DYN_8bit, true, true);
             } else {
-                startIf(jmpReg, (op->inst==SetZ_R8?DYN_EQUALS_ZERO:DYN_NOT_EQUALS_ZERO), true);
+                startIf(jmpReg, (op->inst == SetZ_R8 ? DYN_EQUALS_ZERO : DYN_NOT_EQUALS_ZERO), true);
                 movToCpu(OFFSET_REG8(op->reg), DYN_8bit, 1);
                 startElse();
                 movToCpu(OFFSET_REG8(op->reg), DYN_8bit, 0);
-                endIf();                
+                endIf();
             }
             INCREMENT_EIP(op->len);
             data->skipToOp = op->next;

project/linux/msvc-remote/Boxedwine/Boxedwine/Boxedwine.vcxproj
@@ -4441,7 +4441,7 @@
   </ItemGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|ARM'">
     <ClCompile>
-      <PreprocessorDefinitions>BOXEDWINE_DYNAMIC_ARMV7;BOXEDWINE_DYNAMIC;BOXEDWINE_OPENGL_IMGUI_V2;BOXEDWINE_POSIX;POCO_UTIL_NO_JSONCONFIGURATION;POCO_UTIL_NO_XMLCONFIGURATION;SIMDE_SSE2_NO_NATIVE;BOXEDWINE_ZLIB;GLH=&lt;SDL_opengl.h&gt;;SDL2=1;BOXEDWINE_HAS_SETJMP;BOXEDWINE_OPENGL_SDL;</PreprocessorDefinitions>
+      <PreprocessorDefinitions>BOXEDWINE_DYNAMIC_ARMV7;BOXEDWINE_DYNAMIC;BOXEDWINE_OPENGL_IMGUI_V2;BOXEDWINE_POSIX;POCO_UTIL_NO_JSONCONFIGURATION;POCO_UTIL_NO_XMLCONFIGURATION;SIMDE_SSE2_NO_NATIVE;BOXEDWINE_ZLIB;GLH=&lt;SDL_opengl.h&gt;;SDL2=1;BOXEDWINE_HAS_SETJMP;BOXEDWINE_OPENGL_SDL;_DEBUG</PreprocessorDefinitions>
       <CppLanguageStandard>c++17</CppLanguageStandard>
       <CppAdditionalWarning>no-conversion;switch;no-deprecated-declarations;empty-body;return-type;parentheses;no-format;uninitialized;unreachable-code;unused-function;unused-value;unused-variable;%(CppAdditionalWarning)</CppAdditionalWarning>
       <CAdditionalWarning>switch;no-deprecated-declarations;empty-body;no-conversion;return-type;parentheses;no-pointer-sign;no-format;uninitialized;unreachable-code;unused-function;unused-value;unused-variable;%(CAdditionalWarning)</CAdditionalWarning>

project/linux/msvc-remote/Boxedwine/Boxedwine/Boxedwine.vcxproj.user
@@ -34,6 +34,7 @@
     <PreLaunchCommand>export DISPLAY=:0</PreLaunchCommand>
     <RemoteDebuggerCommand>projects/Boxedwine/project/linux/msvc-remote/Boxedwine/Boxedwine/bin/ARM/Debug/Boxedwine.out</RemoteDebuggerCommand>
     <RemoteDebuggerWorkingDirectory>projects/Boxedwine/project/linux/msvc-remote/Boxedwine/Boxedwine/bin/ARM/Debug</RemoteDebuggerWorkingDirectory>
+    <RemoteDebuggerCommandArguments>-zip /home/pi/.local/share/Boxedwine/FileSystems/Wine-5.0.zip -root /home/pi/.local/share/Boxedwine/Containers/Test-6aeeae0c/root /bin/wine notepad.exe</RemoteDebuggerCommandArguments>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Test|ARM'">
     <RemoteTarget>-902386300;192.168.1.41 (username=, port=22, authentication=Password)</RemoteTarget>

source/emulation/cpu/armv7/armv7CPU.cpp
@@ -15,6 +15,24 @@
 #define OFFSET_REG8(x) (x>=4?offsetof(CPU, reg[x-4].h8):offsetof(CPU, reg[x].u8))
 #define CPU_OFFSET_OF(x) offsetof(CPU, x)
 
+// per instruction, not per block.  
+static bool regUsed[8];
+static bool regWasUsed[8];
+
+void setRegUsed(U8 reg) {
+    regUsed[reg] = true;
+    regWasUsed[reg] = true;
+}
+
+void clearRegUsed(U8 reg) {
+    regUsed[reg] = false;
+}
+
+void resetRegsUsed() {
+    memset(regUsed, 0, sizeof(regUsed));
+    memset(regWasUsed, 0, sizeof(regWasUsed));
+}
+
 // DynReg is a required type, but the values inside are local to this file
 enum DynReg {
     DYN_R0 = 0,
@@ -212,6 +230,9 @@ static std::vector<U32> ifJump;
 #define REG_PC 15
 #define REG_LR 14
 #define REG_SP 13
+
+// save regs R4-R7 and LR
+#define BLOCK_REGS_SAVED 0x41F0
 
 void ensureBufferSize(U32 grow) {
     if (!outBuffer) {
@@ -864,12 +885,12 @@ void codeCreated(U8* start, U8* end) {
 }
 
 void startBlock() {
-    pushRegs(0x41F0); // save regs R4-R7 and LR
+    pushRegs(BLOCK_REGS_SAVED); 
     movToRegFromReg(REG_CPU, DYN_32bit, DYN_R0, DYN_32bit, false);
 }
 
 void endBlock() {
-    popRegs(0x41F0);
+    popRegs(BLOCK_REGS_SAVED);
     rtn();
 }
 
@@ -878,6 +899,44 @@ void callHostFunction(void* address, bool hasReturn, U32 argCount, U32 arg1, Dyn
 #include "../dynamic/dynamic_generic_base.h"
 
 void callHostFunction(void* address, bool hasReturn, U32 argCount, U32 arg1, DynCallParamType arg1Type, bool doneWithArg1, U32 arg2, DynCallParamType arg2Type, bool doneWithArg2, U32 arg3, DynCallParamType arg3Type, bool doneWithArg3, U32 arg4, DynCallParamType arg4Type, bool doneWithArg4, U32 arg5, DynCallParamType arg5Type, bool doneWithArg5) {
+    bool regDone[8] = { false, false, false, false, false, false, false, false };
+
+    if (argCount >= 5) {
+        if (isParamTypeReg(arg5Type) && doneWithArg5) {
+            if (arg5 >= 8)
+                kpanic("armv7::callHostFunction bad param 5: arg=%d argType=%d", arg5, arg5Type);
+            regDone[arg5] = true;
+        }
+    }
+    if (argCount >= 4) {
+        if (isParamTypeReg(arg4Type) && doneWithArg4) {
+            if (arg4 >= 8)
+                kpanic("armv7::callHostFunction bad param 4: arg=%d argType=%d", arg4, arg4Type);
+            regDone[arg4] = true;
+        }
+    }
+    if (argCount >= 3) {
+        if (isParamTypeReg(arg3Type) && doneWithArg3) {
+            if (arg3 >= 8)
+                kpanic("armv7::callHostFunction bad param 3: arg=%d argType=%d", arg3, arg3Type);
+            regDone[arg3] = true;
+        }
+    }
+    if (argCount >= 2) {
+        if (isParamTypeReg(arg2Type) && doneWithArg2) {
+            if (arg2 >= 8)
+                kpanic("armv7::callHostFunction bad param 5: arg=%d argType=%d", arg2, arg2Type);
+            regDone[arg2] = true;
+        }
+    }
+    if (argCount >= 1) {
+        if (isParamTypeReg(arg1Type) && doneWithArg1) {
+            if (arg1 >= 8)
+                kpanic("armv7::callHostFunction bad param 5: arg=%d argType=%d", arg1, arg1Type);
+            regDone[arg1] = true;
+        }
+    }
+
     if (argCount >= 5) {
         pushValue(arg5, arg5Type);
     }
@@ -893,13 +952,23 @@ void callHostFunction(void* address, bool hasReturn, U32 argCount, U32 arg1, Dyn
     if (argCount >= 1) {
         setValue(arg1, arg1Type, 0);
     }
-
+    if (regUsed[DYN_CALL_RESULT] && !hasReturn && !regDone[DYN_CALL_RESULT]) {
+        int ii = 0;
+    }    
     // reg can't be R0-R3 because that is used for params 
     // dyn_src, dyn_dest, dyn_address need to be preserved
     loadConst32(DYN_R8, (U32)address);
     callFunctionReg32(DYN_R8);
     if (argCount >= 5) {
         addValue32(REG_SP, 4);
+    }
+    for (int i = 0; i < 8; i++) {
+        if (regDone[i]) {
+            clearRegUsed(i);
+        }
+    }
+    if (hasReturn) {
+        regUsed[DYN_CALL_RESULT] = true;
     }
 }
 

commit cb15bdc09edf76729e4049adee7cfc28364d7e2a

Fixed a regression in the dynamic core for the sub instruction when it doesn't need to calculated flags.  This was caused by 5e53844 (July 6, 2020)

source/emulation/cpu/dynamic/dynamic_func.h
@@ -119,11 +119,14 @@ void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, Dyn
                 }
                 if (needResultReg) {
                     movFromMem(width, DYN_ADDRESS, !store);
-                    instRegReg(inst, DYN_SRC, DYN_CALL_RESULT, width, true);
+                    instRegReg(inst, DYN_CALL_RESULT, DYN_SRC, width, true);
                     if (store) {
+                        movToRegFromReg(DYN_SRC, width, DYN_CALL_RESULT, width, true);
                         movToMemFromReg(DYN_ADDRESS, DYN_SRC, width, true, false);
-                    }
-                    jmpReg = DYN_SRC;
+                        jmpReg = DYN_SRC;
+                    } else {
+                        jmpReg = DYN_CALL_RESULT;
+                    }                    
                 } else {
                     instMemReg(inst, DYN_ADDRESS, DYN_SRC, width, true, true);
                 }
@@ -185,11 +188,14 @@ void dynamic_arith(DynamicData* data, DecodedOp* op, DynArg src, DynArg dst, Dyn
                     instRegImm('+', DYN_SRC, width, op->imm);
                     if (needResultReg) {
                         movFromMem(width, DYN_ADDRESS, !store);
-                        instRegReg(inst, DYN_SRC, DYN_CALL_RESULT, width, true);
+                        instRegReg(inst, DYN_CALL_RESULT, DYN_SRC, width, true);
                         if (store) {
+                            movToRegFromReg(DYN_SRC, width, DYN_CALL_RESULT, width, true);
                             movToMemFromReg(DYN_ADDRESS, DYN_SRC, width, true, false);
-                        }
-                        jmpReg = DYN_SRC;
+                            jmpReg = DYN_SRC;
+                        } else {
+                            jmpReg = DYN_CALL_RESULT;
+                        }                        
                     } else {
                         instMemReg(inst, DYN_ADDRESS, DYN_SRC, width, true, true);
                     }