6f432b0881e6e1e0db424c2a31585a52a639fcdb
James Bryant <jwbryant72@yahoo.com>
2020-11-07 12:05:38 -0800

    Will now prevent the mouse from moving into the letter box region when in fullscreenAspect mode

--- include/boxedwine.h
+++ include/boxedwine.h
@@ -2,7 +2,7 @@
 #define __BOXEDWINE_H__
 
 #define BOXEDWINE_VERSION_STR "201"
-#define BOXEDWINE_VERSION_DISPLAY "20.1.4"
+#define BOXEDWINE_VERSION_DISPLAY "20.1.5"
 
 #include <string>
 #include <vector>
