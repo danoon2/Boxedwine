07d0a0eb602459341b1f81abf10e855a5a2a60d0
James Bryant <jwbryant72@yahoo.com>
2021-05-07 10:20:25 -0700

    SDL_WINDOW_FULLSCREEN_DESKTOP seems to be the correct full screen flag that works on both Windows and Mac

--- platform/sdl/knativewindow.cpp
+++ platform/sdl/knativewindow.cpp
@@ -708,13 +708,13 @@ void KNativeWindowSdl::displayChanged(KThread* thread) {
             if (fullScreen == FULLSCREEN_STRETCH) {
                 cx = dm.w;
                 cy = dm.h;
-                flags |= SDL_WINDOW_FULLSCREEN;
+                flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
                 scaleX = dm.w * 100 / width;
                 scaleY = dm.h * 100 / height;
             } else if (fullScreen == FULLSCREEN_ASPECT) {
                 cx = dm.w;
                 cy = dm.h;
-                flags |= SDL_WINDOW_FULLSCREEN;
+                flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
                 scaleX = dm.w * 100 / width;
                 scaleY = dm.h * 100 / height;
                 scaleXOffset = 0;
@@ -727,14 +727,14 @@ void KNativeWindowSdl::displayChanged(KThread* thread) {
                     scaleXOffset = (dm.w - width * scaleX / 100) / 2;
                 }
             } else if (width == (U32)dm.w && height == (U32)dm.h) {
-                flags |= SDL_WINDOW_FULLSCREEN;
+                flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
             }   
             if (cx > dm.w || cy > dm.h) {
                 cx = dm.w;
                 cy = dm.h;
                 scaleX = dm.w * 100 / width;
                 scaleY = dm.h * 100 / height;
-                flags |= SDL_WINDOW_FULLSCREEN;
+                flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
             }
         }
         
