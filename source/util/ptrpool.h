aa769f50131d4e7a0b311acc1f89a4026ee545ee
James <jwbryant72@yahoo.com>
2024-01-19 19:32:47 -0800

    soft mmu: forgot to add memory/object pool file

---
+++ source/util/ptrpool.h
@@ -0,0 +1,50 @@
+#pragma once
+
+template<typename T>
+class PtrPool {
+private:
+    std::queue<T*> queue;
+    std::vector<T*> allocated;
+    BOXEDWINE_MUTEX mutex;
+    int blockSize;
+
+public:
+    PtrPool(int blockSize=10000) : blockSize(blockSize) {}
+    ~PtrPool() {deleteAll();}
+
+    /// Return an object from the pool.
+    T* get() {
+        BOXEDWINE_CRITICAL_SECTION_WITH_MUTEX(mutex);
+        T* newData;
+
+        if (!queue.empty()) {
+            newData = queue.front();
+            queue.pop();
+            newData->reset();
+            return newData;
+        }
+        T* t = new T[blockSize];
+        for (int i = 1; i < blockSize; i++) {
+            t[i].reset();
+            queue.push(&t[i]);
+        }
+        t[0].reset();
+        allocated.push_back(t);
+        return &t[0];
+    }
+
+    /// Mark the given object for reuse in the future.
+    inline void put(T* obj) {
+        BOXEDWINE_CRITICAL_SECTION_WITH_MUTEX(mutex);
+        queue.push(obj);
+    }
+
+    void deleteAll() {
+        BOXEDWINE_CRITICAL_SECTION_WITH_MUTEX(mutex);
+        for (auto& t : allocated) {
+            delete[] t;
+        }
+        allocated.clear();
+        queue = {};
+    }
+};
